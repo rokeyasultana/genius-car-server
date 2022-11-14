9c32aa32da969bddf3988789b5b32c88e20090ce
jakiasultana2022 <rokeyasbd@gmail.com>
2022-11-14 20:25:29 +0600

    update-delete

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const { query } = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -20,6 +21,9 @@ async function run (){
   try{
 const serviceCollection = client.db('genius-car').collection('services');
 
+const orderCollection = client.db('genius-car').collection('order');
+
+//services
 
 app.get('/services', async (req, res) => {
   const query = {}
@@ -28,13 +32,82 @@ app.get('/services', async (req, res) => {
   res.send(services);
 });
 
-
+//service id
 app.get('/services/:id',async(req,res)=>{
   const id = req.params.id;
   const query = {_id:ObjectId(id)};
   const service = await serviceCollection.findOne(query);
   res.send(service);
 })
+
+
+//orders post
+
+app.post('/orders',async(req,res)=>{
+const order =req.body;
+const result = await orderCollection.insertOne(order);
+res.send(result);
+});
+
+//orders get
+
+app.get('/orders',async(req,res)=>{
+
+let query = {};
+
+if(req.query.email){
+  query = {
+       email:req.query.email
+  }
+}
+
+  const cursor = orderCollection.find(query);
+  const orders = await cursor.toArray();
+  res.send(orders);
+
+})
+
+
+app.patch('/orders/:id', async (req, res) => {
+  const id = req.params.id;
+  const status = req.body.status
+  const query = { _id: ObjectId(id) }
+  const updatedDoc = {
+      $set:{
+          status: status
+      }
+  }
+  const result = await orderCollection.updateOne(query, updatedDoc);
+  res.send(result);
+})
+
+//delete order
+app.delete('/orders/:id', async (req, res) => {
+  const id = req.params.id;
+  const query = { _id: ObjectId(id) };
+  const result = await orderCollection.deleteOne(query);
+  res.send(result);
+})
+
+//update
+
+app.patch('/orders/:id', async(req,res)=>{
+const id = req.params.id;
+const status = req.body.status;
+const query = {_id: ObjectId(id)};
+const updatedDoc ={
+  $set:{
+    status:status
+  }
+ 
+}
+const result = await orderCollection.updateOne(query,updatedDoc);
+res.send(result);
+
+
+})
+
+
 
 
   }
